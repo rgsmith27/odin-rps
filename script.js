50c82097da070be6faefba2b07d98b492072a04f
rgsmith27 <149701337+rgsmith27@users.noreply.github.com>
2025-07-03 15:53:45 -0400

    add computer choice

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello, World!")
+function getComputerChoice(){
+    let rng = Math.random();
+    if (rng > .66){
+        return "rock";
+    }
+    if (rng > .33){
+        return "paper";
+    }
+    else{
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
