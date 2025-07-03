2b56c7eca4b1cc38fb4eedb5d0bdfa45fa708ce1
rgsmith27 <149701337+rgsmith27@users.noreply.github.com>
2025-07-03 16:21:24 -0400

    add single round and handle invalid choices

--- script.js
+++ script.js
@@ -4,28 +4,88 @@ let humanScore = 0;
 function getComputerChoice(){
     let rng = Math.random();
     if (rng > .66){
-        return "rock";
+        return "ROCK";
     }
     if (rng > .33){
-        return "paper";
+        return "PAPER";
     }
     else{
-        return "scissors";
+        return "SCISSORS";
     }
 }
 
 function getHumanChoice(){
     let rng = Math.random();
+    let response;
     if (rng > .66){
-        return prompt(`The machine's hands move so fast as to be impossible to percieve. There's no telling what your opponent might do. 
+        response = prompt(`The machine's hands move so fast as to be impossible to percieve. There's no telling what your opponent might do. 
             What do you choose?`);
     }
-    if (rng > .33){
-        return prompt(`While you are only playing a JavaScript console, you can still feel its hatred through the blank white background.
+    else if (rng > .33){
+        response = prompt(`While you are only playing a JavaScript console, you can still feel its hatred through the blank white background.
             What do you choose?`);
     }
     else{
-        return prompt(`You must make a decision. The machine has made its long before you were ready.
+        response = prompt(`You must make a decision. The machine has made its long before you were ready.
             What do you choose?";`)
     }
+    response = response.toUpperCase();
+    if(response == "ROCK" || response == "PAPER" || response == "SCISSORS"){
+        return response;
+    }
+    else{
+        console.log("You cannot avoid the choice.");
+        return getHumanChoice();
+    }
 }
+
+function playRound(humanChoice, computerChoice){
+    switch(humanChoice){
+        case "ROCK":
+            switch(computerChoice){
+                case "ROCK":
+                    console.log("ROCK and ROCK, it's a tie!");
+                    break;
+                case "PAPER":
+                    console.log("PAPER beats ROCK, you lose!");
+                    computerScore++;
+                    break;
+                case "SCISSORS":
+                    console.log("ROCK beats SCISSORS, you win!");
+                    humanScore++;
+                    break;
+            }
+            break;
+        case "PAPER":
+            switch(computerChoice){
+                case "ROCK":
+                    console.log("PAPER beats ROCK, you win!");
+                    humanScore++;
+                    break;
+                case "PAPER":
+                    console.log("PAPER and PAPER, it's a tie!");
+                    break;
+                case "SCISSORS":
+                    console.log("SCISSORS beats PAPER, you lose!");
+                    break;
+            }
+            break;
+        case "SCISSORS":
+            switch(computerChoice){
+                case "ROCK":
+                    console.log("ROCK beats SCISSORS, you lose!");
+                    computerScore++;
+                    break;
+                case "PAPER":
+                    console.log("SCISSORS beats PAPER, you win!");
+                    humanScore++;
+                    break;
+                case "SCISSORS":
+                    console.log("SCISSORS and SCISSORS, it's a tie!");
+                    break;
+            }
+            break;
+    }
+}
+
+playRound(getHumanChoice(),getComputerChoice());
