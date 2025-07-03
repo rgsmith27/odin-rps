f7ef2b9991b219c1ede389630f4cec392839ca3d
rgsmith27 <149701337+rgsmith27@users.noreply.github.com>
2025-07-03 16:00:31 -0400

    add human choice (with some extra flavor, of course)

--- script.js
+++ script.js
@@ -11,4 +11,18 @@ function getComputerChoice(){
     }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice(){
+    let rng = Math.random();
+    if (rng > .66){
+        return prompt(`The machine's hands move so fast as to be impossible to percieve. There's no telling what your opponent might do. 
+            What do you choose?`);
+    }
+    if (rng > .33){
+        return prompt(`While you are only playing a JavaScript console, you can still feel its hatred through the blank white background.
+            What do you choose?`);
+    }
+    else{
+        return prompt(`You must make a decision. The machine has made its long before you were ready.
+            What do you choose?";`)
+    }
+}
